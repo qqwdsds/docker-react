3d6237a4fde5bb768d7620e466ddcd861f2a47a1
qqweq <maximligomina007@gmail.com>
2024-10-23 16:31:20 +0000

    q

--- src/App.js
+++ src/App.js
@@ -7,16 +7,14 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+		hihihaha
+	</p>
+        <p
           className="App-link"
-          href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          rel="noopener noreferrer">
+          Learn how to use brain
+        </p>
       </header>
     </div>
   );
